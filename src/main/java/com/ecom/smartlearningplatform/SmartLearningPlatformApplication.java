f9b5d01a4c58e61f088059babe27604ff5edd0d0
Rohit Kumar Chintamani <rohitkumarchintamani@Mac.cable.rcn.com>
2025-12-10 01:47:29 -0600

    Initial commit

---
+++ src/main/java/com/ecom/smartlearningplatform/SmartLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.smartlearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLearningPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecom/smartlearningplatform/SmartLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.smartlearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLearningPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
